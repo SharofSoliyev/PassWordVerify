4b2cf35e74a5619f6e5b1264ed34819edc97aa7a
sharobiddin_thinkland <sharobiddin@thinkland.uz>
2021-04-22 12:34:52 +0500

    Add project files.

---
+++ PassWordVerify/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassWordVerify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string t = txtbx.Text;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "0123456789";
+            string specialChars = "#?!,-'/`_*$";
+            string password = t;
+            int score = 0;
+            int daraja = t.Length;
+
+            if (Contains(password, uppercase))
+            {
+                score += 26; 
+            }
+
+            if (Contains(password, lowercase))
+            {
+                score += 26;
+            }
+
+            if (Contains(password, digits))
+            {
+                score += 10;
+            }
+
+            if (Contains(password, specialChars))
+            {
+                score += 12;
+            }
+
+            BigInteger a = BigInteger.Pow(score, daraja);
+            BigInteger p = a / 1000000;
+            BigInteger big = p / 31536000;
+            natija.Content = big.ToString() + " Yilda buza oladi !";
+
+
+        }
+        public static bool Contains(string target, string list)
+        {
+            return target.IndexOfAny(list.ToCharArray()) != -1;
+        }
+    }
+}
---
+++ PasswordVerifyConsole/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordVerifyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minLength = 8;
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            string digits = "0123456789";
+            string specialChars = "#?!,-'/`_*$";
+            string password = "2woRDsss";
+            int score = 0;
+            if ((score) >= (minLength))
+            {
+                score++;
+            }
+
+            if (Contains(password, uppercase))
+            {
+                score++;
+            }
+
+            if (Contains(password, lowercase))
+            {
+                score++;
+            }
+
+            if (Contains(password, digits))
+            {
+                score++;
+            }
+
+            if (Contains(password, specialChars))
+            {
+                score++;
+            }
+
+            Console.WriteLine(password);
+            Console.WriteLine(score);
+
+            switch (score)
+            {
+                case 5:
+                case 4:
+                    Console.WriteLine("This is an extremely strong password");
+                    break;
+                case 3:
+                    Console.WriteLine("This is a strong password");
+                    break;
+                case 2:
+                    Console.WriteLine("This is an average password");
+                    break;
+                case 1:
+                    Console.WriteLine("This is a weak password");
+                    break;
+                default:
+                    Console.WriteLine("This password doesn't meet the standards");
+                    break;
+            }
+            Console.ReadLine();
+        }
+        public static bool Contains(string target, string list)
+        {
+            return target.IndexOfAny(list.ToCharArray()) != -1;
+        }
+    }
+}
